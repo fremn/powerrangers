441515670cff9f84b7d1d33414a53004258f947f
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-09-10 16:57:05 -0500

    updates the screeam method to out put the persons status and the run method to divide cafination by 3

--- powerranger.rb
+++ powerranger.rb
@@ -3,26 +3,37 @@ class Person
 	
 	def initialize(name)
 		@name = name
-		@caffination = 10.0
+		@caffination = 36.0
 	end	
 
 
 	def run
-
+		@caffination = @caffination/3.0
 	end
 
 	def scream
-
+		puts "My name is #{@name} I have a caffine level of #{@caffination}."
 	end
 
 	def drink_coffee
-		@caffination +=5.0
+		@caffination += 3
 	end
 
 end
 
 #_______________end of Person Class_____________
 
+molly = Person.new(:molly)
+
+molly.scream
+
+molly.run
+molly.run
+molly.run
+molly.scream
+molly.run
+molly.drink_coffee
+molly.scream
 
 
 class PowerRanger
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
-Create the class Person
+[done] Create the class Person
 
-A person has attributes: name and caffeine level
+[done] A person has attributes: name and caffeine level
 
-A person can run, scream and drink_coffee (Be creative with what these methods do)
+[done] A person can run, scream and drink_coffee (Be creative with what these methods do)
+
