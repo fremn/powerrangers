7bbd6aa1af3800a102636312975412bb6ec6797c
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-09-10 16:46:33 -0500

    adds readme which describes what Person class should do an implements these features at a basic level

--- powerranger.rb
+++ powerranger.rb
@@ -1,5 +1,29 @@
 class Person
+	attr_accessor :name, :caffination
+	
+	def initialize(name)
+		@name = name
+		@caffination = 10.0
+	end	
+
+
+	def run
+
+	end
+
+	def scream
+
+	end
+
+	def drink_coffee
+		@caffination +=5.0
+	end
+
 end
+
+#_______________end of Person Class_____________
+
+
 
 class PowerRanger
 end
---
+++ readme.md
@@ -0,0 +1,5 @@
+Create the class Person
+
+A person has attributes: name and caffeine level
+
+A person can run, scream and drink_coffee (Be creative with what these methods do)
